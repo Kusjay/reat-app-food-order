c77fd0dc18afdd549f9a299449ffc4091651c3c9
Kusjay <kelechiuma@gmail.com>
2021-10-24 14:31:35 +0100

    Adding individual meal item and displaying them

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 
 import HeaderCartButton from './HeaderCartButton';
-import mealsImage from '../../assets/meals.jpg';
+import mealsImage from '../../assets/meals2.jpg';
 import classes from './Header.module.css';
 
 const Header = () => {
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+	return (
+		<section className={classes.summary}>
+			<h2>Delicious Food, Delivered To You Fast</h2>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+				aspernatur minus ex, nesciunt voluptas illo!
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum,
+				repellendus quod. Ipsa voluptatibus dicta accusantium.
+			</p>
+		</section>
+	);
+};
+
+export default MealsSummary;
